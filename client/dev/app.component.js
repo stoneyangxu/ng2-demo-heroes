4d27dee9a54cc394b7009451f1bfc961bf6d4726
stoneyangxu <stoneyangxu@icloud.com>
2017-03-05 21:08:26 +0800

    add styles

--- client/dev/app.component.js
+++ client/dev/app.component.js
@@ -22,7 +22,9 @@ AppComponent = __decorate([
      <a routerLink="/heroes">Heroes</a>
    </nav>
     <router-outlet></router-outlet>
-  `
+  `,
+        styleUrls: ["styles/app.component.css"]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
