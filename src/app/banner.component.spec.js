377b40f62a577de16801573227b93900b14e76fa
stoneyangxu <stoneyangxu@icloud.com>
2017-03-08 21:27:02 +0800

    add bannercomponent & test

---
+++ src/app/banner.component.spec.js
@@ -0,0 +1,38 @@
+"use strict";
+var banner_component_1 = require("./banner.component");
+var testing_1 = require("@angular/core/testing");
+var platform_browser_1 = require("@angular/platform-browser");
+/**
+ * Created by yangxu on 2017/3/7.
+ */
+describe("BannerComponent", function () {
+    var comp;
+    var fixture;
+    var de;
+    var el;
+    beforeEach(function () {
+        testing_1.TestBed.configureTestingModule({
+            declarations: [banner_component_1.BannerComponent],
+            providers: [
+                {
+                    provider: testing_1.ComponentFixtureAutoDetect,
+                    useValue: true
+                }
+            ]
+        });
+        fixture = testing_1.TestBed.createComponent(banner_component_1.BannerComponent);
+        comp = fixture.componentInstance;
+        de = fixture.debugElement.query(platform_browser_1.By.css("h1"));
+        el = de.nativeElement;
+    });
+    it("should display original title", function () {
+        fixture.detectChanges();
+        expect(el.textContent).toBe(comp.title);
+    });
+    it("should display a different test title", function () {
+        comp.title = "Test Title";
+        fixture.detectChanges();
+        expect(el.textContent).toBe("Test Title");
+    });
+});
+//# sourceMappingURL=banner.component.spec.js.map
